c47be3cae9800e303a2f12631a15f5232220782f
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2024-12-02 15:35:35 +0100

    add letsgo.twig party array

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 
 {
     #[Route('/')]
     public function letsgo() :Response
     {
-        return new Response("Party");
+        $party1 = ['name'=>'Techno', 'seats'=> 100]; # Fuer die Technoparty sind noch 100 Plaetze frei!
+        $party2 = ['name'=>'Rock', 'seats'=> 10]; # Achtung, fuer die Rockparty sind nur noch 10 Plaetze frei!
+        $party3 = ['name'=>'Hip Hop', 'seats'=> 0]; # Hip Hop Party ist ausverkauft!!!
+
+        #$daten = ['name'=>'Hip Hop','partys'=>['Party 1', 'Party 2', 'Party 3']];
+        $daten = ['name'=>'Hip Hop','partys'=>[$party1, $party2, $party3]];
+
+        return $this->render('main/letsgo.html.twig', $daten);
     }
 
     #[Route('/show/{slug}')]
