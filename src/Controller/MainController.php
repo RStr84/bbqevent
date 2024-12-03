259ce2c61500c3b77b92ce70ca7dbd5416a0ee71
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2024-12-03 15:27:51 +0100

    Event class, EventRepository und show.html.twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Model\Event;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,28 +12,47 @@ class MainController extends AbstractController
 
 {
     #[Route('/')]
-    public function letsgo() :Response
+    public function letsgo(EventRepository $eventRepository): Response
     {
-        $party1 = ['name' => 'Techno', 'seats' => 100]; # Fuer die Technoparty sind noch 100 Plaetze frei!
-        $party2 = ['name' => 'Rock', 'seats' => 10]; # Achtung, fuer die Rockparty sind nur noch 10 Plaetze frei!
-        $party3 = ['name' => 'Hip Hop', 'seats' => 0]; # Hip Hop Party ist ausverkauft!!!
 
-        $daten = ['name' => 'Hip Hop','partys' => ['Party 1', 'Party 2', 'Party 3']];
-        $daten2 = ['name' => 'Hip Hop','partys' => ['Party 1' => $party1, 'Party 2' => $party2, 'Party 3' => $party3]];
+//        $party1 = ['name' => 'Techno', 'seats' => 100]; # Fuer die Technoparty sind noch 100 Plaetze frei!         von 200
+//        $party2 = ['name' => 'Rock', 'seats' => 10]; # Achtung, fuer die Rockparty sind nur noch 10 Plaetze frei!  von 200
+//        $party3 = ['name' => 'Hip Hop', 'seats' => 0]; # Hip Hop Event ist ausverkauft!!!                          von 200
+//
+//        $daten = ['name' => 'Hip Hop','partys' => ['party1', 'party2', 'party3']];
+//        $daten2 = ['name' => 'Hip Hop','partys' => ['party1' => $party1, 'party2' => $party2, 'party3' => $party3]];
+//        $daten3 = ['name' => 'Hip Hop','party1' => $party1];
+        $daten4 = ['name' => 'Hip Hop', 'events' => $eventRepository->findAll()];
 
-        return $this->render('main/letsgo.html.twig', $daten2);
+        return $this->render('main/letsgo.html.twig', $daten4);
 
     }
 
-    #[Route('/show/{slug}')]
-    public function show(string $slug = null) :Response
+    /// 1.Version ///
+//    #[Route('//show/{slug}}')]
+//    public function show(string $slug = null): Response
+//    {
+//        if ($slug) {
+//            $title = ucwords(str_replace('_', ' ', $slug));
+//            return new Response("Zeige uns $title!");
+//        } else {
+//            return new Response('Zeige uns alle Partys!');
+//        }
+//    }
+
+
+    /// 2.Version ///
+    #[Route('/show/{id}')]
+    public function show(string $id = null, EventRepository $eventRepository): Response
     {
-        if ($slug) {
-            $title = ucwords(str_replace('_', ' ', $slug));
-            return new Response("Zeige uns $title!");
-        } else{
-            return new Response("Zeige uns alle Partys!");
+        $event = $eventRepository->findById($id);
+        if($event) {
+            return $this->render('main/show.html.twig', ['event' => $event]);
+        } else {
+            return new Response('Kein Event gefunden');
         }
     }
 
+
 }
+
---
+++ src/Repository/EventRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Event;
+
+class EventRepository
+{
+    public function findAll(): array
+    {
+        $event1 = new Event(1, "Techno", "super Techno Event", "Alex", 50);
+        $event2 = new Event(2, "Rock", "super Rock Event", "Berghain", 190);
+        $event3 = new Event(3, "Hip Hop", "super Hip Hop Event", "The Street", 200);
+
+        return [$event1, $event2, $event3];
+    }
+
+    public function findById($id): Event|null
+    {
+       $events = $this->findAll();
+       foreach ($events as $event) {
+           if ($event->getId() == $id) {
+               return $event;
+           }
+       }
+       return null;
+    }
+}
