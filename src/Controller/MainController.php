987bb5544b0e815e58d4e64a44b62e822601815a
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2024-12-03 09:18:21 +0100

    add letsgo.twig party array

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,14 +12,15 @@ class MainController extends AbstractController
     #[Route('/')]
     public function letsgo() :Response
     {
-        $party1 = ['name'=>'Techno', 'seats'=> 100]; # Fuer die Technoparty sind noch 100 Plaetze frei!
-        $party2 = ['name'=>'Rock', 'seats'=> 10]; # Achtung, fuer die Rockparty sind nur noch 10 Plaetze frei!
-        $party3 = ['name'=>'Hip Hop', 'seats'=> 0]; # Hip Hop Party ist ausverkauft!!!
+        $party1 = ['name' => 'Techno', 'seats' => 100]; # Fuer die Technoparty sind noch 100 Plaetze frei!
+        $party2 = ['name' => 'Rock', 'seats' => 10]; # Achtung, fuer die Rockparty sind nur noch 10 Plaetze frei!
+        $party3 = ['name' => 'Hip Hop', 'seats' => 0]; # Hip Hop Party ist ausverkauft!!!
 
-        $daten = ['name'=>'Hip Hop','partys'=>['Party 1', 'Party 2', 'Party 3']];
-//        $daten = ['name'=>'Hip Hop','partys'=>[$party1, $party2, $party3]];
+        $daten = ['name' => 'Hip Hop','partys' => ['Party 1', 'Party 2', 'Party 3']];
+        $daten2 = ['name' => 'Hip Hop','partys' => ['Party 1' => $party1, 'Party 2' => $party2, 'Party 3' => $party3]];
 
-        return $this->render('main/letsgo.html.twig', $daten);
+        return $this->render('main/letsgo.html.twig', $daten2);
+
     }
 
     #[Route('/show/{slug}')]
