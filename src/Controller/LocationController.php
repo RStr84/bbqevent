3a2028d5b117ad1210d8b0c9a03b23b957b4d5e7
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2024-12-10 12:47:59 +0100

    EventFormType und new.html.twig LocationFormType und new.html.twig

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Location;
+use App\Form\LocationFormType;
 use App\Repository\LocationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -53,18 +55,31 @@ class LocationController extends AbstractController
 
 
     #[Route('/new', name: 'app_location_new')]
-    public function new(EntityManagerInterface $entityManager) : Response
+    public function new(EntityManagerInterface $entityManager, Request $request): Response
     {
         $location = new Location();
-        $location->setName('The Street')
-            ->setCapacity(10000)
-            ->setAddress("Potsdamer Strasse 2, 10785 Berlin");
+        $form = $this->createForm(LocationFormType::class, $location, [
+            'action' => $this->generateUrl('app_location_new'),
+            'method' => 'POST',
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $location = $form->getData();
+            $entityManager->persist($location);
+            $entityManager->flush();
+//        $location->setName('The Street')
+//            ->setCapacity(10000)
+//            ->setAddress("Potsdamer Strasse 2, 10785 Berlin");
 
 //        dd($location);
-        $entityManager->persist($location);
-        $entityManager->flush();
+//        $entityManager->persist($location);
+//        $entityManager->flush();
 //        dd($location);
-        return new Response('Ich bin drin!');
+            return $this->redirectToRoute('app_location_show', ['id' => $location->getId()]);
+        } else {
+            return $this->render('location/new.html.twig', ['form' => $form->createView()]);
+        }
     }
-
 }
