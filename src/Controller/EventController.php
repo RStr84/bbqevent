4b00ee96259970455e94bf4e22add0c0c00e10d7
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2024-12-10 12:17:46 +0100

    EventFormType und new.html.twig

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Form\EventFormType;
 use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -51,33 +52,68 @@ class EventController extends AbstractController
         return $this->redirectToRoute('app_event_index');
     }
 
+//    #[Route('/new', name: 'app_event_new')]
+//    public function new(EntityManagerInterface $entityManager, Request $request) : Response
+//    {
+////        dd($request->isMethod('POST'));
+//        $event = new Event();
+//        $event->setName('Techno')
+//            ->setDescription('super Techno Party')
+//            ->setBookedSeats(150);
+//
+////        dd($event);
+//        $entityManager->persist($event);
+//        $entityManager->flush();
+////        dd($event);
+//        return new Response('Ich bin drin!');
+//    }
+
+
     #[Route('/new', name: 'app_event_new')]
-    public function new(EntityManagerInterface $entityManager, Request $request) : Response
+    public function new(EntityManagerInterface $entityManager, Request $request): Response
     {
-//        dd($request->getHost());
         $event = new Event();
-        $event->setName('Techno')
-            ->setDescription('super Techno Party')
-            ->setBookedSeats(150);
+        $form = $this->createForm(EventFormType::class, $event, [
+            'action' => $this->generateUrl('app_event_new'),
+            'method' => 'POST',
+        ]);
+        $form->handleRequest($request);
 
-//        dd($event);
-        $entityManager->persist($event);
-        $entityManager->flush();
-//        dd($event);
-        return new Response('Ich bin drin!');
-    }
+        if ($form->isSubmitted() && $form->isValid()) {
 
+            $event = $form->getData();
+//            dd($event);
 
-    #[Route('/test', name: 'app_event_test')]
-    public function test(Request $request)
-    {
-        if ($request->isMethod('GET')) {
-            return $this->render('post.html.twig');
-        } elseif ($request->isMethod('POST')) {
-            $var = $request->request->all()['key'];
-            return new Response("Datenbanksachen $var");
+//        if ($request->isMethod('POST')) {
+//            $event->setName($request->request->get('name'))
+//                ->setDescription($request->request->get('description'))
+//                ->setBookedSeats($request->request->get('seats'));
+            $entityManager->persist($event);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
+
+        } else {
+            return $this->render('event/new.html.twig', ['form' => $form->createView()]);
         }
+
     }
 
+
+//    #[Route('/test', name: 'app_event_test')]
+//    public function test(Request $request)
+//    {
+//        if ($request->isMethod('GET')) {
+//            return $this->render('post.html.twig');
+//        } elseif ($request->isMethod('POST')) {
+//            $var = $request->request->all()['key'];
+//            return new Response("Datenbanksachen $var");
+//        }
+//    }
+
+//        #[Route('/test', name: 'app_event_test')]
+//        public function test()
+//        { return new Response('Ich komme von NEW');
+//
+//}
 
 }
